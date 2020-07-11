9caeb138fae0ed9d35ed152f8e7b0ad267127c4a
Daniel Almeida <danielosvaldoal@outlook.com>
2020-07-11 19:24:43 +0100

    Alterações

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,197 +1,289 @@
 <template>
-<div id="home">
-  <div class="carousel slide" id="slide-show" data-ride="carousel">
-        <ul class="carousel-indicators">
-            <li data-target="#slide-show" data-slide-to="0" class="active"></li>
-        </ul>
-        <div class="carousel-inner">
-            <div class="carousel-item active">
-                <img src="../assets/images/login.jpg" alt="" style="width:100%; height:10%">
-            </div>
+  <div id="home">
+    <div class="carousel slide" id="slide-show" data-ride="carousel">
+      <ul class="carousel-indicators">
+        <li data-target="#slide-show" data-slide-to="0" class="active"></li>
+        <li data-target="#slide-show" data-slide-to="1"></li>
+      </ul>
+      <div class="carousel-inner crsl-inner">
+        <div class="carousel-item crsl-item">
+          <img src="../assets/images/slide-1.jpg" alt />
+          <div class="carousel-caption d-none d-md-block crsl-caption-right">
+            <h5>What is Lorem Ipsum</h5>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+          </div>
         </div>
-        <a href="#slide-show" class="carousel-control-prev" data-slide="prev">
-            <span>
-                <i class="fas fa-chevron-left"></i>       
-            </span>    
-        </a>
-        <a href="#slide-show" class="carousel-control-next" data-slide="next">
-            <span>
-                <i class="fas fa-chevron-right"></i>
-            </span>    
-        </a>
+        <div class="carousel-item active crsl-item">
+          <img src="../assets/images/slide-2.jpg" alt />
+          <div class="carousel-caption d-none d-md-block crsl-caption-left">
+            <h5>What is Lorem Ipsum</h5>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+          </div>
+        </div>
+      </div>
+      <a href="#slide-show" class="carousel-control-prev" data-slide="prev">
+        <span>
+          <i class="fas fa-chevron-left"></i>
+        </span>
+      </a>
+      <a href="#slide-show" class="carousel-control-next" data-slide="next">
+        <span>
+          <i class="fas fa-chevron-right"></i>
+        </span>
+      </a>
     </div>
     <div id="tipos-seguros">
-    <div class="row text-white">
+      <div class="row text-white">
         <div class="col-md-3 col-sm-3">
-            <span>
-                <i class="fas fa-heartbeat"></i>
-            </span>
-            <br/>
-            <p class="lead">Cuidamos da sua saúde, lhe apresentando os melhores planos e as suas vantagens</p>
+          <span>
+            <i class="fas fa-heartbeat"></i>
+          </span>
+          <br />
+          <p class="lead">
+            Cuidamos da sua saúde, lhe apresentando os melhores planos e as suas
+            vantagens
+          </p>
         </div>
         <div class="col-md-3 col-sm-3">
-            <span>
-                <i class="fas fa-car-crash"></i>
-            </span>
-            <br/>
-            <p class="lead">Cuidamos do seu automóvel com as diversas modalidades de seguro automóvel</p>
+          <span>
+            <i class="fas fa-car-crash"></i>
+          </span>
+          <br />
+          <p class="lead">
+            Cuidamos do seu automóvel com as diversas modalidades de seguro
+            automóvel
+          </p>
         </div>
         <div class="col-md-3 col-sm-3">
-            <span>
-                <i class="fas fa-people-carry"></i>
-            </span>
-            <p class="lead">Ajudamos a conseguir a melhor assistência caso aconteça algum acidente no desenvolver da sua ativadade laboral</p>
+          <span>
+            <i class="fas fa-people-carry"></i>
+          </span>
+          <p class="lead">
+            Ajudamos a conseguir a melhor assistência caso aconteça algum
+            acidente no desenvolver da sua ativadade laboral
+          </p>
         </div>
         <div class="col-md-3 col-sm-3">
-            <span>
-                <i class="fas fa-building"></i>
-            </span>
-            <p class="lead">Ajudamos a sua Empresa a encontar o melhor seguro e que se adapte a sua realidade para garantir o ciclo de funcionamento de seus equipamentos e a sua produção</p>
+          <span>
+            <i class="fas fa-building"></i>
+          </span>
+          <p class="lead">
+            Ajudamos a sua Empresa a encontar o melhor seguro e que se adapte a
+            sua realidade para garantir o ciclo de funcionamento de seus
+            equipamentos e a sua produção
+          </p>
         </div>
+      </div>
     </div>
-</div>
-<div id="about-us" class="container-fluid">
-    <div class="row" >
+    <div id="about-us" class="container-fluid mt-3 mb-3">
+      <div class="row">
         <div class="col-md-7 col-sm-7" id="desc-us">
+          <div class="d-flex flex-column justify-content-center" style="height: 100%;">
             <h4>Sobre nós</h4>
-            <hr>
+            <!-- <hr /> -->
             <p class="lead">
-            Somos uma empresa de capital privado e inteiramente angolano, fundada em 2018, com profissionais competente, altamente qualificados, pronto e disposto para lhe proporcionar o melhor que o nosso mercado segurador oferece.
-            <h6>Missão e Valores:</h6>
-            <ul>
-                <li class="lead">Capitalizar e aumentar valor para os acionistas;</li>
-                <li class="lead">Contribuir para melhor gerir e assegurar os riscos;</li>
-                <li class="lead">Contribuir no desenvolvimento socioeconômico de Angola;</li>
-                <li class="lead">Proporcionar às empresas os melhores seguros aos riscos que estão exposto;</li>
-                <li class="lead">Assegurar uma excelente gestão das carteira de seguros;</li>
-                <li class="lead">Participar ativamente na educação financeira a nível de seguros em Angola;</li>
-                <li class="lead">Elevar a importância estratégica da equipe laboral e capacitá-los para melhor servir.</li>
-            </ul>
-            <!--<router-link class="btn" id="btn-vermais" to="/servicos"><i class="fas fa-plus-circle"></i> Informacao</router-link>     -->
+              Somos uma empresa de capital privado e inteiramente angolano,
+              fundada em 2018, com profissionais competente, altamente
+              qualificados, pronto e disposto para lhe proporcionar o melhor que o
+              nosso mercado segurador oferece.
+            </p>
+          </div>
+
+          <!--<router-link class="btn" id="btn-vermais" to="/servicos"><i class="fas fa-plus-circle"></i> Informacao</router-link>     -->
         </div>
         <div class="col-md-5 col-sm-5" style="padding:0">
-            <img src="../assets/images/2.jpg" alt="">
+          <img src="../assets/images/2.jpg" alt />
         </div>
+      </div>
     </div>
-</div>
-<div  id="parceiros">
-    <h4 style="text-align:right">Parceiros</h4>
-    <hr>
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-md-4 col-sm-4">
-                <img src="../assets/images/bic.jpg" alt="">
-            </div>
-            <div class="col-md-4 col-sm-4">
-                <img src="../assets/images/nossa.png" alt="">
-            </div>
-            <div class="col-md-4 col-sm-4">
-                <img src="../assets/images/gobal.png" alt="">
-            </div>
+    <!-- <hr /> -->
+    <div id="servicos" class="container-fluid p-5 mb-3">
+      <h4 class="text-center">Serviços</h4>
+      <div>
+        <div class="row text-white text-center">
+          <div class="col-md-3 col-sm-3">
+            <span>
+              <i class="fas fa-heartbeat"></i>
+            </span>
+            <br />
+            <p class="lead">Consultoria</p>
+          </div>
+          <div class="col-md-3 col-sm-3">
+            <span>
+              <i class="fas fa-car-crash"></i>
+            </span>
+            <br />
+            <p class="lead">Análise de Risco</p>
+          </div>
+          <div class="col-md-3 col-sm-3">
+            <span>
+              <i class="fas fa-people-carry"></i>
+            </span>
+            <p class="lead">Gestão da carteira de Seguro</p>
+          </div>
+          <div class="col-md-3 col-sm-3">
+            <span>
+              <i class="fas fa-building"></i>
+            </span>
+            <p class="lead">Renovação do Seguro</p>
+          </div>
         </div>
-        <div class="row">
-            <div class="col-md-4 col-sm-4">
-                <img src="../assets/images/saham.jpg" alt="" style="width:300px">
-                
-            </div>
-            <div class="col-md-4 col-sm-4">
-                <img src="../assets/images/fidelidade.jpg" alt="" style="width:200px">
-            </div>
-            <div class="col-md-4">
-                
-            </div>
-        </div>
+      </div>
     </div>
-</div>
-</div>
+    <!-- <hr /> -->
+    <div id="parceiros" class="container-fluid p-3">
+      <h4 style="text-align:center">Parceiros</h4>
+      <div>
+        <div class="row">
+          <div class="col-md-3 col-sm-3 parceiros-card">
+            <img src="../assets/images/nossa.png" alt />
+          </div>
+          <div class="col-md-3 col-sm-3 parceiros-card">
+            <img src="../assets/images/bic.jpg" alt />
+          </div>
+          <div class="col-md-3 col-sm-3 parceiros-card">
+            <img src="../assets/images/gobal.png" alt />
+          </div>
+          <div class="col-md-3 col-sm-3 parceiros-card">
+            <img src="../assets/images/saham.jpg" alt />
+          </div>
+          <div class="col-md-3 col-sm-3 parceiros-card">
+            <img src="../assets/images/fidelidade.jpg" alt />
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {
- 
-}
+export default {};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-#home{
-    padding: 0;
-    border: 2px solid transparent;
-    z-index: -1;
-    
-}
-#slide-show{
-    margin-top: 5.3%;
-}
-#tipos-seguros{
-    background-color: #8EC03B;
-    padding: 3%;
-}
-#tipos-seguros span{
-    font-size: 80px;
-    margin-left: 110px;
-}
-#tipos-seguros p{
-    text-align: center;
-}
-#about-us img{
-   padding: 0;
-   margin: 0;
-   width: 100%;
-   height: 500px;
-}
-#about-us p{
-   padding: 2% 2% 0 2%;
-   text-align: justify;
-}
-#desc-us{
-    padding: 2% 2% 0 2%;
-}
-h4{
-    text-transform: uppercase;
-    color:#8EC03B;
-    font-family: ubuntu;
-}
-#parceiros{
-    padding: 1% 0.5% 1% 0;
-}
-#parceiros img{
-    width: 100%;
-}
- @media only screen and (max-width: 768px){
-     #slide-show{
-         margin-top:0;
-         width: 100%;
-     }
-     #tipos-seguros{
-        background-color: #8EC03B;
-        padding: 3%;
-    }
-#tipos-seguros span{
-    font-size: 70px;
-    margin-left: 118px;
-}
-#tipos-seguros p{
-    text-align: center;
-    font-size: 16px;
-}
-#about-us p, #about-us ul li{
-   font-size: 16px;
-}
-#desc-us{
-    padding: 2% 2% 0 2%;
-}
-h4{
-    text-transform: uppercase;
-    color:#8EC03B;
-    font-family: ubuntu;
-}
-#parceiros{
-    padding: 1% 0.5% 1% 0;
-}
-#parceiros img{
-    width: 100%;
+.parceiros-card {
+  height: 200px;
 }
 
+#servicos {
+  background: #2b2f36;
+}
+#servicos span {
+  font-size: 80px;
+  color: #8ec03b;
+}
+#servicos p {
+  font-weight: 400; /* margin-left: 110px; */
+}
+.parceiros-card img {
+  height: 100% !important;
+  object-fit: contain !important;
+  /* filter: contrast(0.7); */
+  width: 100% !important;
+}
+
+.crsl-caption-left {
+  left: 50% !important;
+}
+
+.crsl-caption-right {
+  right: 50% !important;
+}
+
+.crsl-inner {
+  height: 550px !important;
+}
+.crsl-item {
+  height: 100% !important;
+}
+
+.crsl-item img {
+  height: 100% !important;
+  object-fit: cover !important;
+  filter: contrast(0.7);
+  width: 100% !important;
+}
+
+#home {
+  padding: 0;
+  border: 2px solid transparent;
+  z-index: -1;
+}
+#slide-show {
+  margin-top: 5.3%;
+}
+#tipos-seguros {
+  background-color: #8ec03b;
+  padding: 3%;
+}
+#tipos-seguros span {
+  font-size: 80px;
+  margin-left: 110px;
+}
+
+#tipos-seguros p {
+  text-align: center;
+}
+#about-us img {
+  padding: 0;
+  margin: 0;
+  width: 100%;
+  /* height: 500px; */
+}
+#about-us p {
+  /* padding: 2% 2% 0 2%; */
+  text-align: justify;
+}
+#desc-us {
+  padding: 2% 2% 0 2%;
+}
+h4 {
+  text-transform: uppercase;
+  color: #8ec03b;
+  font-family: ubuntu;
+  font-weight: bold;
+}
+#parceiros {
+  padding: 1% 0.5% 1% 0;
+}
+#parceiros img {
+  width: 100%;
+}
+@media only screen and (max-width: 768px) {
+  #slide-show {
+    margin-top: 0;
+    width: 100%;
   }
+  #tipos-seguros {
+    background-color: #8ec03b;
+    padding: 3%;
+  }
+  #tipos-seguros span {
+    font-size: 70px;
+    margin-left: 118px;
+  }
+  #tipos-seguros p {
+    text-align: center;
+    font-size: 16px;
+  }
+  #about-us p,
+  #about-us ul li {
+    font-size: 16px;
+  }
+  #desc-us {
+    padding: 2% 2% 0 2%;
+  }
+  h4 {
+    text-transform: uppercase;
+    color: #8ec03b;
+    font-family: ubuntu;
+  }
+  #parceiros {
+    padding: 1% 0.5% 1% 0;
+  }
+  #parceiros img {
+    width: 100%;
+  }
+}
 </style>
--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -1,156 +1,198 @@
 <template>
-<div id="teste">
+  <div id="teste">
     <div class="navbar navbar-expand-sm" id="main_menu">
-        <a href="#" class="navbar-brand">
-            <img src="../assets/images/1.jpg" alt="">
-        </a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#meu_links" id="btn_collapse">
-            <span><i class="fas fa-bars"></i></span>
-        </button>
-        <div id="meu_links" class="collapse navbar-collapse">
-            <ul class="navbar-nav">
-            <li class="nav-item"><router-link to="/" class="nav-link activo" v-on:click.native="ativar"><i class="fas fa-home"></i> Início</router-link></li>
-            <li class="nav-item"><a href="#about-us" class="nav-link"><i class="fas fa-heartbeat"></i> Sobre nós</a></li>
-            <li class="nav-item"><router-link to="/servicos" class="nav-link"><i class="fas fa-cogs"></i> Serviços</router-link></li>
-            <li class="nav-item"><router-link to="/servicos" class="nav-link"><i class="fas fa-hands-helping"></i> Parceiros</router-link></li>
-            <li class="nav-item"><router-link to="/servicos" class="nav-link"><i class="fas fa-camera"></i> Galeria</router-link></li>
-            <li class="nav-item"><router-link to="/servicos" class="nav-link"><i class="fas fa-sign-in-alt"></i> Login</router-link></li>
+      <a href="#" class="navbar-brand" style="margin-left: 3%;">
+        <img src="../assets/images/logo.png" alt style="width: 30%;object-fit: contain;" />
+      </a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#meu_links"
+        id="btn_collapse"
+      >
+        <span>
+          <i class="fas fa-bars"></i>
+        </span>
+      </button>
+      <div id="meu_links" class="collapse navbar-collapse flex-row-reverse">
+        <ul class="navbar-nav" id="header-menu">
+          <li class="nav-item">
+            <router-link to="/" class="nav-link activo" v-on:click.native="ativar">Início</router-link>
+          </li>
+          <li class="nav-item">
+            <a href="#about-us" class="nav-link">Sobre nós</a>
+          </li>
+          <li class="nav-item">
+            <a href="#servicos" class="nav-link">Serviços</a>
+          </li>
+          <li class="nav-item">
+            <a href="#parceiros" class="nav-link">Parceiros</a>
+          </li>
+          <li class="nav-item">
+            <router-link to="/servicos" class="nav-link">
+              <i class="fas fa-sign-in-alt"></i> Login
+            </router-link>
+          </li>
         </ul>
-        </div>  
+      </div>
     </div>
 
-    <router-view name='aux'></router-view>
+    <router-view name="aux"></router-view>
 
-
-   <footer class="container-fluid text-white">
-    <div class="row">
+    <footer class="container-fluid text-white">
+      <div class="row">
         <div class="col-md-3">
-            <ul>
-                <li>Servicos</li>
-                <li><a href="#">&#10095; Consultoria</a></li>
-                <li><a href="#">&#10095; Análise de Risco</a></li>
-                <li><a href="#">&#10095; Gestão da carteira de Seguro</a></li>
-                <li><a href="#">&#10095; Renovação do Seguro</a></li>
-                <li><a href="#">&#10095; Defensor</a></li>
-                <li><a href="#">&#10095; Organização</a></li>
-            </ul>
-        </div>
-        <div class="col-md-3 ">
-            <ul>
-                <li>Termos</li>
-                <li><a href="">&#10095; Termos e condicoes</a></li>
-                <li><a href="#">&#10095; Politicas de reembolso</a></li>
-            </ul>
+          <ul>
+            <li>Servicos</li>
+            <li>&#10095; Consultoria</li>
+            <li>&#10095; Análise de Risco</li>
+            <li>&#10095; Gestão da carteira de Seguro</li>
+            <li>&#10095; Renovação do Seguro</li>
+            <li>&#10095; Defensor</li>
+            <li>&#10095; Organização</li>
+          </ul>
         </div>
         <div class="col-md-3">
-            <ul>
-                <li>C&JS - Seguros</li>
-                <li><a href="#"><i class="fas fa-map-marker-alt" style="color: #dd4a40"></i> Onde estamos</a></li>
-            </ul>
-            
+          <ul>
+            <li>Contactos</li>
+            <li>
+              <i class="fas fa-phone" style="color: #dd4a40"></i>
+              991 139 311
+            </li>
+            <li>
+              <i class="fas fa-phone" style="color: #dd4a40"></i>
+              928 724 627
+            </li>
+          </ul>
         </div>
         <div class="col-md-3">
-            <ul>
-                <li>Midias Sociais</li>
-                <li><a href="X"><i class="fab fa-facebook"></i> Facebook/CJSeguros</a></li>
-                <li><a href="#"><i class="fab fa-instagram"></i> _CSseguros</a></li>
-            </ul>
+          <ul>
+            <li>Onde estamos</li>
+            <li>
+              <i class="fas fa-map-marker-alt" style="color: #dd4a40"></i>
+              Centralidade do Kilamba Edifício K2
+            </li>
+          </ul>
         </div>
-    </div>
-</footer>
-</div>
+        <div class="col-md-3">
+          <ul>
+            <li>Midias Sociais</li>
+            <li>
+              <a href="https://www.facebook.com/cjaseguro/">
+                <i class="fab fa-facebook"></i> CJA-Mediação de Seguros
+              </a>
+            </li>
+            <!-- <li>
+              <a href="#">
+                <i class="fab fa-instagram"></i> _CSseguros
+              </a>
+            </li>-->
+          </ul>
+        </div>
+      </div>
+    </footer>
+  </div>
 </template>
 <script>
-    export default{
-        methods:{
-            ativar: function(){
-                var menuItemAtivo = document.querySelectorALL('nav-item')
-                for(var i=0; i<menuItemAtivo.length; i++){
-                    menuItemAtivo[i].remove('activo')
-                }
-                menuItemAtivo.classList.add('activo')
-            }
-        }
-    }
-</script>
-<style  scoped>
-    @font-face{
-        font-family: 'ubuntu';
-        src: url('../assets/fonts/Ubuntu-Light.ttf');
-    } 
-    #main_menu{
-        background: #fff;
-        border-bottom: 1px solid #8EC03B;
-        z-index: 100;
-        margin: 0;
-        padding: 0;
-        position: fixed;
-        width: 100%;
-        height: 12%;
-    }
-    .navbar-brand{
-        margin-left: 5%;
-    }
-    .navbar-nav{
-        margin-left: 12%;
-    }
-    #btn_collapse{
-        border:1px solid #8EC03B;
-        background: #fff;
-        color:#8EC03B;
-    }
-    .nav-item{
-        margin-left: .5%;
-    }
-    .nav-link{
-        color: rgba(0, 0, 0, 0.8) ;
-        font-size:18px;
-        width: 135px;
-        text-align: center;
-        font-family: ubuntu;
-    }
-    .nav-link:hover,.activo{
-        background-color: #8EC03B;
-        color:#fff;
-        border-radius: 20px;
-    }  
-    footer{
-        background: #2B2F36;
-    }
-    footer>.row{
-        padding: 3%;
-    }
-    footer ul{
-        list-style-type: none;
-    }
-    footer>ul>li>a{
-        color:#fff
-    }
-    footer ul li a:hover{
-        text-decoration: none;
-        color: #8EC03B;
-    }
-/**Media query */
-  @media only screen and (max-width: 768px){
-      #main_menu{
-          position: relative;
-          width: 100%;
+export default {
+  methods: {
+    ativar: function() {
+      var menuItemAtivo = document.querySelectorALL("nav-item");
+      for (var i = 0; i < menuItemAtivo.length; i++) {
+        menuItemAtivo[i].remove("activo");
       }
-      #main_menu .navbar-brand{
-          margin-left: 0;
-          border: 1px solid red;
-          display:none
-      }
-        .navbar-nav{
-        margin-left: 0;
-        }
-            .nav-item{
-        padding: 1%;
-    }
-        .nav-link{
-        font-size:10px;
-        width: 100px;
-        text-align: justify;
+      menuItemAtivo.classList.add("activo");
     }
   }
+};
+</script>
+<style  scoped>
+#header-menu {
+  margin-right: 5%;
+}
+
+@font-face {
+  font-family: "ubuntu";
+  src: url("../assets/fonts/Ubuntu-Light.ttf");
+}
+#main_menu {
+  background: #fff;
+  border-bottom: 1px solid #8ec03b;
+  z-index: 100;
+  margin: 0;
+  padding: 0;
+  position: fixed;
+  width: 100%;
+  height: 12%;
+}
+.navbar-brand {
+  margin-left: 5%;
+}
+.navbar-nav {
+  margin-left: 12%;
+}
+#btn_collapse {
+  border: 1px solid #8ec03b;
+  background: #fff;
+  color: #8ec03b;
+}
+.nav-item {
+  margin-left: 0.5%;
+}
+.nav-link {
+  color: rgba(0, 0, 0, 0.8);
+  font-size: 18px;
+  width: 135px;
+  text-align: center;
+  font-family: ubuntu;
+}
+
+.activo {
+  background-color: #8ec03b;
+  color: #fff !important;
+  border-radius: 20px;
+}
+.nav-link:hover {
+  color: #8ec03b;
+}
+footer {
+  background: #2b2f36;
+}
+footer > .row {
+  padding: 3%;
+}
+footer ul {
+  list-style-type: none;
+}
+footer > ul > li > a {
+  color: #fff;
+}
+footer ul li a:hover {
+  text-decoration: none;
+  color: #8ec03b;
+}
+/**Media query */
+@media only screen and (max-width: 768px) {
+  #main_menu {
+    position: relative;
+    width: 100%;
+  }
+  #main_menu .navbar-brand {
+    margin-left: 0;
+    border: 1px solid red;
+    display: none;
+  }
+  .navbar-nav {
+    margin-left: 0;
+  }
+  .nav-item {
+    padding: 1%;
+  }
+  .nav-link {
+    font-size: 10px;
+    width: 100px;
+    text-align: justify;
+  }
+}
 </style>
